a5645793c8b6ed3fb3b3ce1d504b06fa8c888f05
Deepakgadhave9527 <deepak.gadhave9527@gmail.com>
2024-07-12 15:57:21 +0530

    coding round

---
+++ 2024/102_Coding_round.js
@@ -0,0 +1,67 @@
+//=================================================================================
+
+async function async1() {
+    console.log("async1 start");
+    await async2();
+    console.log("async1 end");
+  }
+  
+  async function async2() {
+    console.log("async2");
+  }
+  
+  console.log("script start");
+  
+  setTimeout(function () {
+    console.log("setTimeout");
+  }, 0);
+  
+  async1();
+  
+  new Promise(function (resolve) {
+    console.log("promise1");
+    resolve();
+  }).then(function () {
+    console.log("promise2");
+  });
+  
+  console.log("script end");
+
+//=================================================================================
+
+  console.log(null == undefined);
+//=================================================================================
+
+  console.log('5' - - '3');
+
+  //=================================================================================
+
+  var a = 10;
+  {
+      var a = -10;
+  }
+  console.log(a);
+
+//=================================================================================
+  const str = "32003023234400340";
+  const countOfZero = [...str].reduce((count, char) => count + (char === '0'), 0);
+  console.log(countOfZero);
+    
+//=================================================================================
+
+  console.log('start');
+
+function sleep(ms) {
+  return new Promise(res => setTimeout(res, ms));
+}
+
+async function delayTime() {
+  console.log("wait for 5 second");
+  await sleep(5000);
+  console.log("stop");
+}
+
+delayTime();
+
+//=================================================================================
+
